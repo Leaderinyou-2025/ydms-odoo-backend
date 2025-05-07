97b93310b5a7a476b635453fea4701925fd5f147
unknown <nguyenmanhphu.vn@gmail.com>
2025-05-07 15:46:41 +0700

    Re-format some models and add Avatar model

---
+++ ydms/models/share_directories/avatar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#    Copyright (C) 2025 LeaderInYou
+#    Author: Oceantech Team
+#############################################################################
+from odoo import api, fields, models, tools, _
+from odoo.addons.base_import.models.base_import import ImportValidationError
+from odoo.exceptions import UserError, ValidationError
+
+class YDMSAvatar(models.Model):
+	_name = 'liy.ydms.avatars'
+	_description = u'Thư viện hình đại diện'
+	_inherit = ['image.mixin']
+	_order = 'name asc'
+
+	name = fields.Char(string=u'Tên hình đại diện', required=True)
+	tags = fields.Char(string=u'Từ khoá', required=True)
