ae6150f20879519c838dccf503a224b27f3155a8
unknown <nguyenmanhphu.vn@gmail.com>
2025-05-07 16:05:34 +0700

    Add Users model empty'

--- ydms/models/base/users.py
+++ ydms/models/base/users.py
@@ -8,18 +8,9 @@ from odoo.addons.base_import.models.base_import import ImportValidationError
 from odoo.exceptions import UserError, ValidationError
 
 
-class YDMSPartner(models.Model):
-	_description = u'YDMSPartner'
-	_inherit = ['res.partner']
-	_order = 'nickname asc'
+class YDMSUsers(models.Model):
+	_description = u'YDMS Users'
+	_inherit = ['res.users']
+	_order = 'name asc'
 
-	nickname = fields.Char(string=u'Biệt danh', tracking=True)
-	is_board_manager = fields.Boolean(u'Lãnh đạo nhà trường', default = False)
-	is_teacher = fields.Boolean(u'Giáo viên', default = False)
-	is_teenager = fields.Boolean(u'Học sinh', default = False)
-	is_parent = fields.Boolean(u'Cha mẹ/Người bảo trợ', default = False)
-	is_expert = fields.Boolean(u'Chuyên gia', default = False)
-
-	social_id = fields.Char(string=u'Số CCCD')
-	edu_id = fields.Char(string=u'Mã định danh của HS')
-	teenager_code = fields.Char(string=u'Mã nội bộ HS')
+	is_board_manager = fields.Boolean(related='partner_id.is_board_manager')
