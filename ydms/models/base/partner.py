9b47f766a19a6ed0ac20f6113ae361ea01dd1245
unknown <nguyenmanhphu.vn@gmail.com>
2025-05-07 13:47:22 +0700

    Add new base, need update module via command

---
+++ ydms/models/base/partner.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#    Copyright (C) 2025 LeaderInYou
+#    Author: Oceantech Team
+#############################################################################
+from odoo import api, fields, models, tools, _
+from odoo.addons.base_import.models.base_import import ImportValidationError
+from odoo.exceptions import UserError, ValidationError
+
+
+class YDMSPartner(models.Model):
+	_description = u'YDMSPartner'
+	_inherit = ['res.partner']
+	_order = 'nickname asc'
+
+	nickname = fields.Char(string=u'Biệt danh', tracking=True)
+	is_board_manager = fields.Boolean(u'Lãnh đạo nhà trường', default = False)
+	is_teacher = fields.Boolean(u'Giáo viên', default = False)
+	is_teenager = fields.Boolean(u'Học sinh', default = False)
+	is_parent = fields.Boolean(u'Cha mẹ/Người bảo trợ', default = False)
+	is_expert = fields.Boolean(u'Chuyên gia', default = False)
+
+	social_id = fields.Char(string=u'Biệt danh')
+	edu_id = fields.Char(string=u'Biệt danh')
+	teenager_code = fields.Char(string=u'Biệt danh')
