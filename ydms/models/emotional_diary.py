0bb6bda3c3464ea021e3c0eeb823062815b08a0e
TRUNGPC <vuthanhtrung666@gmail.com>
2025-05-13 10:13:59 +0700

    feat: 16816,16818,16819 - tạo view cho model "Câu hỏi cảm xúc", "Câu hỏi cảm xúc - Các phương án trả lời câu hỏi", "Nhật ký cảm xúc"

--- ydms/models/emotional_diary.py
+++ ydms/models/emotional_diary.py
@@ -13,6 +13,7 @@ class YDMSEmotionalDiary(models.Model):
 	_description = u'Nhật ký cảm xúc'
 	_inherit = ['mail.thread']
 	_order = 'create_date desc'
+	_rec_name = 'display_name'
 
 	teenager_id = fields.Many2one('res.users', string=u'Học sinh', required=True)
 	nickname = fields.Char(related='teenager_id.partner_id.nickname')
@@ -23,4 +24,9 @@ class YDMSEmotionalDiary(models.Model):
 	answer_icon = fields.Image(string=u'Biểu tượng trả lời', related='answer_id.image_1920')
 	rank_point = fields.Integer(related='question_id.rank_point')
 	scores = fields.Integer(related='answer_id.scores')
+	display_name = fields.Char(string='Tên hiển thị', compute='_compute_display_name', store=False)
 
+	@api.depends('nickname', 'question_id.name', 'scores')
+	def _compute_display_name(self):
+		for rec in self:
+			rec.display_name = f"{rec.teenager_id.name or 'Không rõ'} - {rec.question_id.name or ''} - {rec.scores or 0} điểm"
